3b9572a7dc5c9bf7736db5dfbc421c070dfb2fa2
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-25 19:27:41 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './index.css'
+
+export default function App() {
+  return(
+    <main>
+      <Headers />
+    </main>
+  )
+}
+
+function Headers(){
+  const [IPaddress, setIPaddress] = useState()
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(IPaddress === "") {
+      alert("do not blank");
+      return;
+    }
+    alert(IPaddress)
+  }
+  return(
+    <header>
+      <h1>IP Address Tracker</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={IPaddress} onChange={(e) => setIPaddress(e.target.value)} placeholder='Enter your IP Address here'/>
+        <button type="submit">
+          <img src="images/icon-arrow.svg" alt="arrow icon" />
+        </button>
+      </form>
+
+      <div className='infoWrap'>
+        <div className='info'>
+          <p>IP ADDRESS</p>
+          <p>123.123.23.123</p>
+        </div>
+        <div className='info'>
+          <p>LOCATION</p>
+          <p>Indonesia</p>
+        </div>
+        <div className='info'>
+          <p>TIMEZONE</p>
+          <p>+7 GMT</p>
+        </div>
+        <div className='info'>
+          <p>ISP</p>
+          <p>indiHome</p>
+        </div>
+      </div>
+    </header>
+  );
+}
